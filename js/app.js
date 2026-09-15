bda9fa183be7767c657538944e7a6d25ab54f975
deusgrego1 <deusgrego1.8@proton.me>
2026-09-15 20:51:26 -0300

    Notify input event with alert
    Added an alert to notify when input is received.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const campo = document.querySelector("textarea");
 
     campo.addEventListener("input", () => {
-        localStorage.setItem("prioridade_teste", campo.value);
+        alert("INPUT FUNCIONOU!");
     });
 
 });
